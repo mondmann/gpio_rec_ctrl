a6090bb586f28ce4e6b45b0b6b0150c2d13f393d
E. Loos <15816637+mondmann@users.noreply.github.com>
2022-03-03 21:17:10 +0100

    erste Versuche

--- gpio_rec_ctrl.py
+++ gpio_rec_ctrl.py
@@ -1,81 +1,68 @@
 #!/usr/bin/python3 -u
-
+import asyncio
 import datetime
-import subprocess
 import os
 import logging as log
-from gpiozero import LED, Button
-from time import sleep
-from threading import Thread, Timer
-from typing import Optional
+from enum import Enum
 
-log.basicConfig(level=log.INFO)
+from gpiozero import LED
+from apgpio import GPIO
+
+log.basicConfig(level=log.DEBUG)
 
 # TODO: add config file
 config = dict(
     led_number=8,
     button_number=7,
+    bit_rate=128,  # lame --abr param
+    max_recording_time=180,
+    target_directory="/srv/gpiorec",
+    block_size=4096,  # FIXME: use page size from "getconf PAGESIZE"
 )
 
-button = Button(7)
 
-max_recoding_time = 180 # min
-bit_rate = 128 # lame --abr param
-buffer_size = "150M" # mbuffer 
-target_directory = '/srv/gpiorec/'
-
-
-class ButtonHandler():
-    __whenButtonCallback = None
-
-    def __init__(self, pin, whenButtonCallback):
-        # whenMotionCallback is an async function
-        self.__whenMotionCallback = whenMotionCallback
-
-        # Just init the sensor with gpiozero lib
-        motionSensor = MotionSensor(pin)
-
-        # Method to call when motion is detected
-        motionSensor.when_motion = self.whenMotion
-
-    def whenMotion(self):
-        # Create new asyncio loop
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        future = asyncio.ensure_future(self.__executeWhenMotionCallback()) # Execute async method
-        loop.run_until_complete(future)
-        loop.close()
-
-    async def __executeWhenMotionCallback(self):
-        await self.__whenMotionCallback()
-
-
-class Led_driver:
-    def __init__(self, scheme="ready"):
-        self.running = True
-        self.led_schemes = {
-        "ready": (2,0.05,0.1,0.05),
-        "busy": (0.2,0.2),
-        "record": (0.5,2),
-        "error": (0.05,0.05),
-        }
-        self.set_scheme(scheme)
-        self._led = LED(config["led_number"])
-
-    def stop(self):
-        log.debug(f"{type(self).__name__} stopped" )
+class ButtonHandler:
+    def __init__(self, callback, pin: int = config["button_number"], loop=None):
+        self._button = GPIO(pin, loop)
+        self.loop = loop
+        self.callback = callback
         self.running = False
 
     async def run(self):
+        self.running = True
+        while self.running:
+            if await self._button.read_async() and not await self._button.read_async():
+                await self.callback()
+                await asyncio.sleep(1)  # bounce protection
+
+
+class LedDriver:
+    def __init__(self, scheme: str = "ready"):
+        self.running = False
+        self.led_schemes = {
+            "ready": (2, 0.05, 0.1, 0.05),
+            "busy": (0.2, 0.2),
+            "record": (0.5, 2),
+            "error": (0.05, 0.05),
+        }
+        self.scheme = scheme
+        self._led = LED(config["led_number"])
+
+    def stop(self):
+        log.debug(f"{type(self).__name__} stopped")
+        self.running = False
+
+    async def run(self):
+        self.running = True
         self._led.off()
         while self.running:
             for delay in self._scheme:
-                if self._new_scheme: 
+                if self._new_scheme:
                     self._led.off()
                     self._new_scheme = False
                     break
-                asyncio.sleep(delay)
-                if self._new_scheme: 
+                await asyncio.sleep(delay)
+                if self._new_scheme:
                     self._led.off()
                     self._new_scheme = False
                     break
@@ -96,113 +83,165 @@ class Led_driver:
     scheme = property(fget=get_scheme, fset=set_scheme)
 
 
-class Recorder:
-    def __init__(self, target_directory=os.getcwd()):
-        self.target_directory = target_directory
-        self.recording = False
-        self.error = False
-        self.wavrecord = self.buffer = self.lame = None
-
-    def run(self): # blocking, use in Thread if necessary
-        if self.recording:
-            log.debug(f"{type(self).__name__} ignore start, already running")
-            return # ignore graciously
-        else:
-            log.debug(f"{type(self).__name__} set up recording")
-            outfilename = os.path.join(self.target_directory,
-                    datetime.datetime.now().replace(microsecond=0).isoformat()
-                    .replace('T', '--').replace(':', "-") + ".mp3")
-            self.recording = True
-            self.wavrecord = subprocess.Popen(
-                    "arecord --quiet --format=dat --file-type=raw".split(),
-                    stdout=subprocess.PIPE)
-            self.buffer = subprocess.Popen(
-                    f"mbuffer -q -m {buffer_size}".split(),
-                    stdout=subprocess.PIPE,stdin=self.wavrecord.stdout) 
-            self.lame = subprocess.Popen(
-                    f"lame --quiet -r --abr {bit_rate} -".split() + [outfilename], 
-                    stdin=self.buffer.stdout, stdout=subprocess.PIPE)
-            self.wavrecord.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-            output, err = self.lame.communicate()
-            log.debug(f"{type(self).__name__} started recording")
-            self.wavrecord.wait()
-            self.buffer.wait()
-            self.lame.wait()
-            self.error = not all([0 == x.returncode for x in [self.wavrecord, self.buffer, self.lame]])
-            log.log(log.ERROR if self.error  else log.DEBUG,
-                    f"{type(self).__name__} subprocesses terminated  (" +
-                    f"record: {self.wavrecord.returncode}, " +
-                    f"buffer: {self.buffer.returncode}, " +
-                    f"lame: {self.lame.returncode})")
-            self.recording = False
+class Subprocess:  # abstract
+    def __init__(self):
+        self.subprocess = None
 
     def stop(self):
-        """ stops recording by sendig a SIGTERM to arecord """
-        log.debug(f"{type(self).__name__} terminating wavrecord")
-        self.wavrecord.terminate()
+        """ stops recording by sending a SIGTERM to arecord """
+        if self.subprocess:
+            log.debug(f"{type(self).__name__} terminating subprocess")
+            self.subprocess.terminate()
+
+
+class Recorder(Subprocess):
+    def __init__(self, queue: asyncio.Queue):
+        super(Recorder, self).__init__()
+        self.queue = queue
+        self.subprocess = None
+        self.recording = False
+        self.error = False
+
+    async def run(self):
+        log.debug(f"{type(self).__name__} set up recording")
+        self.error = False
+        self.recording = True
+        self.subprocess = await asyncio.create_subprocess_shell(
+            "arecord --quiet --format=dat --file-type=raw",
+            stdout=asyncio.subprocess.PIPE
+        )
+        log.debug(f"{type(self).__name__} started recording")
+
+        while True:
+            data = await self.subprocess.stdout.read(config['block_size'])
+            self.queue.put_nowait(data)
+            if not data:
+                break  # end of data
+
+        await self.subprocess.wait()
+        self.error = not 0 == self.subprocess.returncode
+        log.log(log.ERROR if self.error else log.DEBUG,
+                f"{type(self).__name__} subprocesses terminated (return code {self.subprocess.returncode}")
+        self.recording = False
+
+
+
+class Encoder(Subprocess):
+    def __init__(self, queue: asyncio.Queue, target_directory: str):
+        super(Encoder, self).__init__()
+        self.queue = queue
+        self.target_directory = target_directory
+        self.subprocess = None
+        self.error = False
+
+    async def run(self):
+        log.debug(f"{type(self).__name__} set up encoding")
+        self.error = False
+        outfilename = os.path.join(self.target_directory,
+                                   datetime.datetime.now().replace(microsecond=0).isoformat()
+                                   .replace('T', '--').replace(':', "-") + ".mp3")
+        self.subprocess = await asyncio.create_subprocess_shell(
+            f"lame --quiet -r --abr {config['bit_rate']} - '{outfilename}'",
+            stdin=asyncio.subprocess.PIPE
+        )
+        log.debug(f"{type(self).__name__} started encoding")
+
+        while True:
+            data = await self.queue.get()
+            self.subprocess.stdin.write(data)
+            await self.subprocess.stdin.drain()
+            if not data:
+                break  # end of data
+
+        self.subprocess.stdin.close()
+        await self.subprocess.stdin.wait_closed()
+        await self.subprocess.wait()
+        self.error = not 0 == self.subprocess.returncode
+        log.log(log.ERROR if self.error else log.DEBUG,
+                f"{type(self).__name__} subprocesses terminated (return code {self.subprocess.returncode}")
+
 
 class Watchdog:
     """
-        compare state of led_driver with recorder (consistency check)
-        check for timeout and stop recording 
+        check for timeout and stop recording if necessary
     """
-    def __init__(self, recorder: Optional[Recorder] = None, led_driver: Optional[Led_driver] = None, timeout=5400):
-        self.recorder = recorder
-        self.led_driver = led_driver
-        self.timeout = timeout # in seconds
-        self.start_time = None
 
-    def run(self):
-        if not self.recorder or not self.led_driver:
-            log.warning(f"{type(self).__name__} " +
-                    f"recorder {'registered' if self.recorder else 'missing'}, "+
-                    f"led_driver {'registered' if self.led_driver else 'missing'}")
-            return # nothing to do
-                     
-        pass # TODO
+    def __init__(self, timeout, callback):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.create_task(self._job())
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        if asyncio.iscoroutine(self._callback):
+            await self._callback()
+        else:
+            self._callback()
+
+    def cancel(self):
+        self._task.cancel()
+
+
+class State(Enum):
+    IDLE = 0
+    RECORDING = 1
+    WRITING = 2
+    ERROR = 42
+
+
+class Controller:
+    def __init__(self):
+        self.led_driver = None
+        self.button_handler = None
+        self.recorder = None  #
+        self.encoder = None
+        self.watchdog = None
+        self.state = State.IDLE
+
+    async def run(self):
+        self.led_driver = LedDriver()  # running continuously
+        # loop ist noch accessible in constructor!
+        self.button_handler = ButtonHandler(self.handle_button_press, loop=asyncio.get_event_loop())
+        await asyncio.gather(self.led_driver.run(), self.button_handler.run())
+
+    async def do_recording(self):
+        queue = asyncio.Queue()
+        self.recorder = Recorder(queue)
+        self.encoder = Encoder(queue, config['target_directory'])
+        self.watchdog = Watchdog(config['max_recording_time'], self.stop_recording)
+        self.led_driver.scheme = "record"
+        self.state = State.RECORDING
+        await asyncio.gather(self.recorder.run(), self.encoder.run())
+        if self.encoder.error or self.recorder.error:
+            self.led_driver.scheme = "error"
+            self.state = State.ERROR
+        else:
+            self.led_driver.scheme = "ready"
+            self.state = State.IDLE
+        assert queue.empty()
+
+    async def stop_recording(self):
+        self.recorder.stop()
+        self.led_driver.scheme = "busy"
+        self.state = State.WRITING
+
+    async def handle_button_press(self):
+        log.debug(f"{type(self).__name__} button press in state {self.state}")
+        if self.state == State.ERROR:
+            return  # ignore button in error state
+        if self.state == State.IDLE:
+            await self.do_recording()
+        if self.state == State.RECORDING:
+            await self.stop_recording()
+        if self.state == State.WRITING:
+            return  # ignore until done
+
 
 def main():
     log.info("starting...")
-    led_driver = Led_driver()
-    led_thread = Thread(target=lambda : led_driver.run())
-    led_thread.start()
+    asyncio.run(Controller().run())
+    log.info("shutdown...")
 
-    watchdog = Watchdog(led_driver=led_driver)
-    watchdog_thread = Thread(target=lambda : do_every(3, watchdog.run()))
-    watchdog_thread.start()
 
-    try:
-        recording = False
-        while True:
-            log.debug("waiting for button press")
-            button.wait_for_press()
-            button.wait_for_release()
-            log.debug("button pressed")
-            if not recording: # start it
-                log.debug("start recording...")
-                recorder = Recorder(target_directory) 
-                recorder_thread = Thread(target=lambda : recorder.run())
-                recorder_thread.start()
-            else: # stop it
-                log.debug("stop recording...")
-                led_driver.scheme = "busy"
-                recorder.stop()
-                retval = recorder_thread.join() # FIXME check retval
-            recording = not recording
-            log.info("recording " + ("started" if recording else "stopped"))
-            led_driver.scheme = "record" if recording else "ready"
-            sleep(1)
-    except KeyboardInterrupt:
-        led_driver.scheme = "busy"
-        recorder.stop()
-        recorder_thread.join()
-        led_driver.stop()
-        watchdog_thread.stop()
-        led_thread.join(3)
-        watchdog_thread.join(3)
-
-# TODO: Watchdog Thread with
-# - check for recording too long
-# - check for recording stopped with error
-
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
